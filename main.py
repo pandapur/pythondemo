012bc8f36f181a6d6a9d305304fe0ec73eafc6bf
Panda Pur <pandapur@pandaland.com>
2023-04-07 02:54:04 +0200

    add remaining days txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 
 DATE_FORMAT_GERMAN = '%d.%m.%Y %H:%M:%S'
+REMAINING_DAYS_TXT = 'Es verbleiben {} Tage'
 
 
 def get_date():
@@ -8,5 +9,11 @@ def get_date():
     return date.strftime(DATE_FORMAT_GERMAN)
 
 
+def get_remaining_days():
+    day_of_year = datetime.datetime.now().strftime('%j')
+    return 365 - int(day_of_year)
+
+
 if __name__ == '__main__':
     print(get_date())
+    print(REMAINING_DAYS_TXT.format(get_remaining_days()))
